092e2a2429ef50b4e97b08bb1730384701f9668c
Kavil-Jain-514 <kaviljain14@gmail.com>
2023-02-15 15:29:50 -0800

    seekBar functionality

--- app/src/main/java/edu/fullerton/kj/colormaker/MainActivity.kt
+++ app/src/main/java/edu/fullerton/kj/colormaker/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.SeekBar
-import android.widget.Toast
 import androidx.appcompat.widget.SwitchCompat
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var resetButton: Button
@@ -28,6 +28,9 @@ class MainActivity : AppCompatActivity() {
         redSwitchCallback()
         blueSwitchCallback()
         greenSwitchCallback()
+        redSeekBarCallback()
+        greenSeekBarCallback()
+        blueSeekBarCallback()
     }
 
     private fun connectViewPointers() {
@@ -94,5 +97,63 @@ class MainActivity : AppCompatActivity() {
                 blueEditText.isEnabled = false
             }
         }
+    }
+
+    var rescaledValue = 0.00392156862
+    var newValue: Double = 0.00
+    var startNum = 0
+    var endNum = 0
+    private fun redSeekBarCallback() {
+        redSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                newValue = p1*rescaledValue
+                val roundOffValue = (newValue * 100.0).roundToInt() / 100.0
+                redEditText.setText(roundOffValue.toString())
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                startNum = redSeekBar.progress
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                endNum = redSeekBar.progress
+            }
+        })
+    }
+
+    private fun greenSeekBarCallback() {
+        greenSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                newValue = p1*rescaledValue
+                val roundOffValue = (newValue * 100.0).roundToInt() / 100.0
+                greenEditText.setText(roundOffValue.toString())
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                startNum = greenSeekBar.progress
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                endNum = greenSeekBar.progress
+            }
+        })
+    }
+
+    private fun blueSeekBarCallback() {
+        blueSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                newValue = p1*rescaledValue
+                val roundOffValue = (newValue * 100.0).roundToInt() / 100.0
+                blueEditText.setText(roundOffValue.toString())
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                startNum = blueSeekBar.progress
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                endNum = blueSeekBar.progress
+            }
+        })
     }
 }
